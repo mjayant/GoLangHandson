2a19a2380579293f2f368d55e0341b7f9ffede5d
Jayant Mishra <jayant.m.1988@gmail.com>
2024-06-16 18:28:31 +0530

    Changes for strconv interface

--- function/main.go
+++ function/main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"math"
+	"strconv"
 )
 
 type square struct {
@@ -30,6 +31,20 @@ type shape interface {
 func info(a1 shape) float64 {
 	a2 := a1.area()
 	return a2
+}
+
+type book struct {
+	title string
+}
+
+func (b book) String() string {
+	return fmt.Sprint("The title of the book is ", b.title)
+}
+
+type count int
+
+func (c count) String() string {
+	return fmt.Sprint("This is the number ", strconv.Itoa(int(c)))
 }
 func main() {
 	// Example for interface
@@ -71,6 +86,16 @@ func main() {
 
 	x()
 	y("Todd")
+
+	// Example for strconv interface or string method
+	b := book{
+		title: "West With The Night",
+	}
+
+	var n count = 42
+
+	fmt.Println(b)
+	fmt.Println(n)
 	// func foo() int {
 	// 	return 07
 	// }
