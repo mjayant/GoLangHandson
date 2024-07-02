70c4ce20dd9399bae5c66ebe0e7db59da242b84c
Jayant Mishra <jayant.m.1988@gmail.com>
2024-07-02 19:03:57 +0530

    Added few more example for struct

--- handson_ex_1/main.go
+++ handson_ex_1/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
 func main() {
 	// Iota example
@@ -32,4 +35,48 @@ func main() {
 	fmt.Println(" A2 values housl be zero", a2)
 	a1, a4, a3 := 1, 2, 3
 	fmt.Printf("Value of a1 is %d , a4 %d and a3 %d", a1, a4, a3)
+
+	// Exercise for random number and string
+	for i := 0; i <= 100; i++ {
+		fmt.Println("Randome number:", rand.Intn(100))
+	}
+	var min int = 500
+	var max int = 1000
+	fmt.Println("Random number from 500 to 1000")
+	for i := 0; i <= 100; i++ {
+		fmt.Println("Randome number:", min+rand.Intn(max-min))
+	}
+
+	p1 := Person{
+		name: "Mishra",
+		age:  34,
+	}
+	p2 := Person{
+		name: "Prerna",
+		age:  32,
+	}
+	e1 := Employee{
+		Person:     p1,
+		department: "Development",
+	}
+	e2 := Employee{
+		Person:     p2,
+		department: "HR",
+	}
+	e1.getName()
+	e2.getName()
+}
+
+type Person struct {
+	name string
+	age  int
+}
+
+type Employee struct {
+	Person
+	department string
+}
+
+func (p1 Person) getName() {
+	fmt.Println("Person name is ", p1.name)
 }
